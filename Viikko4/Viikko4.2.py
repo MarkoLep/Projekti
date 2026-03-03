a353accbe9adee6100ef340f0eff7bc3eb812569
Markolep <marklepa@metropolia.fi>
2026-03-03 12:47:39 +0200

    Viikko3

---
+++ Viikko4/Viikko4.2.py
@@ -0,0 +1,6 @@
+tuumat = float(input("anna tuumat:", ))
+while tuumat > 0:
+    print(tuumat * 2.4, "cm")
+    tuumat = float(input("anna tuumat:", ))
+if tuumat < 0:
+        print("Anna positiivinen luku")
